7b01789eedeb3e72dd305fa9649af978673b6ad0
Chris Finney and Nathaniel Meyer <chris@>
2017-02-21 14:51:04 -0800

    Final revision

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
   $("form#transportation-survey").submit(function(event){
     $("#work-responses").show();
+    $("#play-responses").show();
+
     $("input:checkbox[name=work-transportation]:checked").each(function(){
       var workTransportationMode = $(this).val();
       $("#work-responses").append(workTransportationMode + "<br>");
     });
 
+    $("input:checkbox[name=play-transportation]:checked").each(function(){
+      var playTransportationMode = $(this).val();
+      $("#play-responses").append(playTransportationMode + "<br>");
+    });
 
     event.preventDefault();
   });
